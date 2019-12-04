5094693860d995c047731c4370bda2b4528c9f25
Knua <dusdn10tkf@korea.ac.kr>
2019-12-04 05:49:35 -0800

    Bug Hunting

--- echo_server/echo_server.cpp
+++ echo_server/echo_server.cpp
@@ -10,16 +10,64 @@
 #include <mutex> // for lock
 #include <thread>
 
+using std::thread;
+
 bool b_opt_check = false;
 void usage() {
     printf("syntax: echo_server <port> [-b]\n");
     printf("sample: echo_server 1234 -b\n");
 }
 
+void echo_thread(int sockfd, std::set<int> socket_descriptor, std::mutex &m){
+
+	m.lock();
+	struct sockaddr_in addr;
+	socklen_t clientlen = sizeof(sockaddr);
+	int childfd = accept(sockfd, reinterpret_cast<struct sockaddr*>(&addr), &clientlen);
+	
+	if (childfd < 0) {
+		perror("ERROR on accept");
+	}
+	else{
+		printf("connected\n");
+		socket_descriptor.insert(childfd);
+		m.unlock();
+
+		while (true) {
+			const static int BUFSIZE = 1024;
+			char buf[BUFSIZE];
+
+			ssize_t received = recv(childfd, buf, BUFSIZE - 1, 0);
+			if (received == 0 || received == -1) {
+				perror("recv failed");
+				break;
+			}
+			buf[received] = '\0';
+			printf("%s\n", buf);
+
+			int checksum = 0;
+			if(b_opt_check){
+				for(std::set<int>::iterator it = socket_descriptor.begin(); it != socket_descriptor.end(); it++){
+					checksum += send(*it, buf, strlen(buf), 0);
+				}
+			}
+			else checksum += send(childfd, buf, strlen(buf), 0);
+			if (checksum == 0) {
+				perror("send failed");
+				break;
+			}
+		}
+
+		m.lock();
+		socket_descriptor.erase(childfd);
+		m.unlock();
+	}
+}
+
 int main(int argc, char  * argv[]) 
 {
-	int Sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (Sockfd == -1) {
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1) {
 		perror("socket failed");
 		return -1;
 	}
@@ -37,7 +85,7 @@ int main(int argc, char  * argv[])
     int port = atoi(argv[1]);
 
 	int optval = 1;
-	setsockopt(Sockfd, SOL_SOCKET, SO_REUSEADDR,  &optval , sizeof(int));
+	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR,  &optval , sizeof(int));
 
 	struct sockaddr_in addr;
 	addr.sin_family = AF_INET;
@@ -45,68 +93,24 @@ int main(int argc, char  * argv[])
 	addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	memset(addr.sin_zero, 0, sizeof(addr.sin_zero));
 
-	int res = bind(Sockfd, reinterpret_cast<struct sockaddr*>(&addr), sizeof(struct sockaddr));
+	int res = bind(sockfd, reinterpret_cast<struct sockaddr*>(&addr), sizeof(struct sockaddr));
 	if (res == -1) {
 		perror("bind failed");
 		return -1;
 	}
 
-	res = listen(Sockfd, 2); // backlog
+	res = listen(sockfd, 2); // backlog
 	if (res == -1) {
 		perror("listen failed");
 		return -1;
 	}
 
 	std::mutex M;
-	std::set<int> socket_Descriptor;
+	std::set<int> socket_descriptor;
 	while (true) {
-		std::thread echo([](int sockfd, std::set<int> socket_descriptor, std::mutex &m) {
-
-			m.lock();
-			struct sockaddr_in addr;
-			socklen_t clientlen = sizeof(sockaddr);
-			int childfd = accept(sockfd, reinterpret_cast<struct sockaddr*>(&addr), &clientlen);
-				
-			if (childfd < 0) {
-				perror("ERROR on accept");
-				return -1;
-			}
-			printf("connected\n");
-			socket_descriptor.insert(childfd);
-			m.unlock();
-
-			while (true) {
-				const static int BUFSIZE = 1024;
-				char buf[BUFSIZE];
-
-				ssize_t received = recv(childfd, buf, BUFSIZE - 1, 0);
-				if (received == 0 || received == -1) {
-					perror("recv failed");
-					break;
-				}
-				buf[received] = '\0';
-				printf("%s\n", buf);
-
-				int checksum = 0;
-				if(b_opt_check){
-					for(std::set<int>::iterator it = socket_descriptor.begin(); it != socket_descriptor.end(); it++){
-						checksum += send(*it, buf, strlen(buf), 0);
-					}
-				}
-				else checksum += send(childfd, buf, strlen(buf), 0);
-				if (checksum == 0) {
-					perror("send failed");
-					break;
-				}
-			}
-
-			m.lock();
-			socket_descriptor.erase(childfd);
-			m.unlock();
-
-		}, Sockfd, socket_Descriptor, &M);
-		echo.detach();
+		thread echo = thread(echo_thread, std::ref(sockfd), std::ref(socket_descriptor), std::ref(M));
+		// echo.detach();
 	}
 
-	close(Sockfd);
+	close(sockfd);
 }
